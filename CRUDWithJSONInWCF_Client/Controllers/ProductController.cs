c0cc28354cfe8c4ff14ef4b203959224656aee37
sampanna pokhrel <sampanna99@gmail.com>
2016-11-06 21:40:41 -0600

    complete CRUD functionality

--- CRUDWithJSONInWCF_Client/Controllers/ProductController.cs
+++ CRUDWithJSONInWCF_Client/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using CRUDWithJSONInWCF_Client.Models;
+using CRUDWithJSONInWCF_Client.ViewModels;
 using System.Web.Mvc;
 
 namespace CRUDWithJSONInWCF_Client.Controllers
@@ -11,6 +12,49 @@ namespace CRUDWithJSONInWCF_Client.Controllers
             ProductServiceClient psc = new ProductServiceClient();
             ViewBag.listProducts = psc.findAll();
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return View("Create");
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductViewModel pvm)
+        {
+            ProductServiceClient psc = new ProductServiceClient();
+            psc.create(pvm.Product);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(string id)
+        {
+
+            ProductServiceClient psc = new ProductServiceClient();
+            psc.delete(psc.find(id));
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+
+            ProductServiceClient psc = new ProductServiceClient();
+            ProductViewModel pvm = new ProductViewModel();
+            pvm.Product = psc.find(id);
+            psc.edit(psc.find(id));
+            return View("Edit", pvm);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProductViewModel pvm)
+        {
+
+            ProductServiceClient psc = new ProductServiceClient();
+            psc.edit(pvm.Product);
+            return RedirectToAction("Index");
         }
     }
 }
